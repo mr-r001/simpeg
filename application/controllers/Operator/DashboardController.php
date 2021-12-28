30be367f3536820a6d62e8308d64115e0cb74150
Ryan1604 <Ryan.Aprianto77777@gmail.com>
2021-12-28 12:52:48 +0700

    Dashboard Done

--- application/controllers/Operator/DashboardController.php
+++ application/controllers/Operator/DashboardController.php
@@ -19,9 +19,20 @@ class DashboardController extends CI_Controller
 				'title' => "Dashboard"
 			);
 
-			$data['dosen'] 						= $this->db->query('SELECT * FROM users WHERE role = 2')->num_rows();
-			$data['pegawai'] 					= $this->db->query('SELECT * FROM users WHERE role = 3')->num_rows();
-			$data['operator']		 			= $this->db->query('SELECT * FROM users WHERE role = 4')->num_rows();
+			$data['tetap'] 				= $this->db->query("SELECT * FROM personal_dosen WHERE id_dosen=1")->num_rows();
+			$data['tidak_tetap'] 		= $this->db->query("SELECT * FROM personal_dosen WHERE id_dosen=2")->num_rows();
+			$data['sesuai'] 			= $this->db->query("SELECT * FROM personal_dosen WHERE id_dosen_skills=1")->num_rows();
+			$data['tidak_sesuai'] 		= $this->db->query("SELECT * FROM personal_dosen WHERE id_dosen_skills=2")->num_rows();
+			$data['teknisi'] 			= $this->db->query("SELECT * FROM personal_pegawai WHERE id_kategori=5")->num_rows();
+			$data['laboran'] 			= $this->db->query("SELECT * FROM personal_pegawai WHERE id_kategori=6")->num_rows();
+			$data['administrasi'] 		= $this->db->query("SELECT * FROM personal_pegawai WHERE id_kategori=7")->num_rows();
+			$data['arsiparis'] 			= $this->db->query("SELECT * FROM personal_pegawai WHERE id_kategori=8")->num_rows();
+
+			$data['tetap_sesuai'] 		= $this->db->query("SELECT * FROM personal_dosen WHERE id_dosen=1 AND id_dosen_skills=1")->num_rows();
+			$data['tetap_tidak_sesuai'] = $this->db->query("SELECT * FROM personal_dosen WHERE id_dosen=1 AND id_dosen_skills=2")->num_rows();
+
+			$data['tidak_tetap_sesuai'] = $this->db->query("SELECT * FROM personal_dosen WHERE id_dosen=2 AND id_dosen_skills=1")->num_rows();
+			$data['tidak_tetap_tidak_sesuai'] = $this->db->query("SELECT * FROM personal_dosen WHERE id_dosen=2 AND id_dosen_skills=2")->num_rows();
 			$this->load->view('pages/Admin/dashboard/index.php', $data);
 		} else {
 			redirect('/');
--- application/controllers/Pegawai/DashboardController.php
+++ application/controllers/Pegawai/DashboardController.php
@@ -19,6 +19,20 @@ class DashboardController extends CI_Controller
 				'title' => "Dashboard"
 			);
 			$id = $this->session->userdata('id');
+			$data['tetap'] 				= $this->db->query("SELECT * FROM personal_dosen WHERE id_dosen=1")->num_rows();
+			$data['tidak_tetap'] 		= $this->db->query("SELECT * FROM personal_dosen WHERE id_dosen=2")->num_rows();
+			$data['sesuai'] 			= $this->db->query("SELECT * FROM personal_dosen WHERE id_dosen_skills=1")->num_rows();
+			$data['tidak_sesuai'] 		= $this->db->query("SELECT * FROM personal_dosen WHERE id_dosen_skills=2")->num_rows();
+			$data['teknisi'] 			= $this->db->query("SELECT * FROM personal_pegawai WHERE id_kategori=5")->num_rows();
+			$data['laboran'] 			= $this->db->query("SELECT * FROM personal_pegawai WHERE id_kategori=6")->num_rows();
+			$data['administrasi'] 		= $this->db->query("SELECT * FROM personal_pegawai WHERE id_kategori=7")->num_rows();
+			$data['arsiparis'] 			= $this->db->query("SELECT * FROM personal_pegawai WHERE id_kategori=8")->num_rows();
+
+			$data['tetap_sesuai'] 		= $this->db->query("SELECT * FROM personal_dosen WHERE id_dosen=1 AND id_dosen_skills=1")->num_rows();
+			$data['tetap_tidak_sesuai'] = $this->db->query("SELECT * FROM personal_dosen WHERE id_dosen=1 AND id_dosen_skills=2")->num_rows();
+
+			$data['tidak_tetap_sesuai'] = $this->db->query("SELECT * FROM personal_dosen WHERE id_dosen=2 AND id_dosen_skills=1")->num_rows();
+			$data['tidak_tetap_tidak_sesuai'] = $this->db->query("SELECT * FROM personal_dosen WHERE id_dosen=2 AND id_dosen_skills=2")->num_rows();
 			$this->load->view('pages/Pegawai/dashboard/index.php', $data);
 		} else {
 			redirect('/');
